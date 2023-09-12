f8d8fac1724c78107d5441f7410279906e1851b7
prsnnami <prasannamishra50@gmail.com>
2023-09-11 23:09:40 -0400

    table features

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,219 @@ import DataTable from "./Datatable";
 import "./index.css";
 
 const dataTableData = [
-  { name: "John Doe", age: 30, occupation: "Software Engineer" },
-  { name: "Jane Doe", age: 28, occupation: "Data Scientist" },
-  { name: "Bob Smith", age: 35, occupation: "Product Manager" },
-  { name: "Alice Johnson", age: 32, occupation: "UX Designer" },
-  { name: "Charlie Brown", age: 29, occupation: "Web Developer" },
-  { name: "David Williams", age: 40, occupation: "System Analyst" },
-  { name: "Eva Green", age: 34, occupation: "Database Administrator" },
-  { name: "Frank Miller", age: 36, occupation: "Network Engineer" },
-  { name: "Grace Hopper", age: 42, occupation: "Software Architect" },
-  { name: "Helen Parker", age: 38, occupation: "IT Manager" },
-  { name: "Ivan Petrov", age: 33, occupation: "Security Specialist" },
-  { name: "Jackie Chan", age: 37, occupation: "Mobile Developer" },
-  { name: "Kevin Hart", age: 39, occupation: "Data Analyst" },
-  { name: "Liam Neeson", age: 41, occupation: "Business Analyst" },
-  { name: "Mia Wallace", age: 35, occupation: "Quality Assurance" },
-  { name: "Nina Simone", age: 43, occupation: "Product Owner" },
-  { name: "Oscar Wilde", age: 44, occupation: "Scrum Master" },
-  { name: "Paul McCartney", age: 45, occupation: "DevOps Engineer" },
-  { name: "Quincy Jones", age: 46, occupation: "Technical Writer" },
-  { name: "Rihanna Fenty", age: 47, occupation: "SEO Specialist" },
-  { name: "Steve Jobs", age: 48, occupation: "Entrepreneur" },
-  { name: "Tina Turner", age: 49, occupation: "Project Manager" },
-  { name: "Uma Thurman", age: 50, occupation: "Software Tester" },
-  { name: "Vin Diesel", age: 51, occupation: "System Administrator" },
+  {
+    name: "John Doe",
+    age: 30,
+    occupation: "Software Engineer",
+    experience: 5,
+    location: "New York",
+  },
+  {
+    name: "Jane Doe",
+    age: 28,
+    occupation: "Data Scientist",
+    experience: 3,
+    location: "San Francisco",
+  },
+  {
+    name: "Bob Smith",
+    age: 35,
+    occupation: "Product Manager",
+    experience: 7,
+    location: "Chicago",
+  },
+  {
+    name: "Alice Johnson",
+    age: 32,
+    occupation: "UX Designer",
+    experience: 4,
+    location: "Seattle",
+  },
+  {
+    name: "Charlie Brown",
+    age: 29,
+    occupation: "Web Developer",
+    experience: 2,
+    location: "Austin",
+  },
+  {
+    name: "David Williams",
+    age: 40,
+    occupation: "System Analyst",
+    experience: 10,
+    location: "Boston",
+  },
+  {
+    name: "Eva Green",
+    age: 34,
+    occupation: "Database Administrator",
+    experience: 6,
+    location: "Los Angeles",
+  },
+  {
+    name: "Frank Miller",
+    age: 36,
+    occupation: "Network Engineer",
+    experience: 8,
+    location: "Denver",
+  },
+  {
+    name: "Grace Hopper",
+    age: 42,
+    occupation: "Software Architect",
+    experience: 12,
+    location: "San Diego",
+  },
+  {
+    name: "Helen Parker",
+    age: 38,
+    occupation: "IT Manager",
+    experience: 9,
+    location: "Dallas",
+  },
+  {
+    name: "Ivan Petrov",
+    age: 33,
+    occupation: "Security Specialist",
+    experience: 5,
+    location: "Phoenix",
+  },
+  {
+    name: "Jackie Chan",
+    age: 37,
+    occupation: "Mobile Developer",
+    experience: 7,
+    location: "San Jose",
+  },
+  {
+    name: "Kevin Hart",
+    age: 39,
+    occupation: "Data Analyst",
+    experience: 8,
+    location: "Jacksonville",
+  },
+  {
+    name: "Liam Neeson",
+    age: 41,
+    occupation: "Business Analyst",
+    experience: 10,
+    location: "San Francisco",
+  },
+  {
+    name: "Mia Wallace",
+    age: 35,
+    occupation: "Quality Assurance",
+    experience: 6,
+    location: "Indianapolis",
+  },
+  {
+    name: "Nina Simone",
+    age: 43,
+    occupation: "Product Owner",
+    experience: 11,
+    location: "Columbus",
+  },
+  {
+    name: "Oscar Wilde",
+    age: 44,
+    occupation: "Scrum Master",
+    experience: 12,
+    location: "Fort Worth",
+  },
+  {
+    name: "Paul McCartney",
+    age: 45,
+    occupation: "DevOps Engineer",
+    experience: 13,
+    location: "Charlotte",
+  },
+  {
+    name: "Quincy Jones",
+    age: 46,
+    occupation: "Technical Writer",
+    experience: 14,
+    location: "Detroit",
+  },
+  {
+    name: "Rihanna Fenty",
+    age: 47,
+    occupation: "SEO Specialist",
+    experience: 15,
+    location: "El Paso",
+  },
+  {
+    name: "Steve Jobs",
+    age: 48,
+    occupation: "Entrepreneur",
+    experience: 16,
+    location: "Memphis",
+  },
+  {
+    name: "Tina Turner",
+    age: 49,
+    occupation: "Project Manager",
+    experience: 17,
+    location: "Boston",
+  },
+  {
+    name: "Uma Thurman",
+    age: 50,
+    occupation: "Software Tester",
+    experience: 18,
+    location: "Seattle",
+  },
+  {
+    name: "Vin Diesel",
+    age: 51,
+    occupation: "System Administrator",
+    experience: 19,
+    location: "Denver",
+  },
+];
+
+const columns = [
+  {
+    id: 1,
+    name: "Name",
+    sortable: true,
+    accessor: "name",
+  },
+  {
+    id: 2,
+    name: "Age",
+    accessor: "age",
+  },
+  {
+    id: 3,
+    name: "Occupation",
+    accessor: "occupation",
+  },
+  {
+    id: 4,
+    name: "Experience",
+    accessor: "experience",
+    render: (x: any) =>
+      x.experience > 6
+        ? "Senior"
+        : x.experience > 3
+        ? "Intermediate"
+        : "Junior",
+  },
+  {
+    id: 5,
+    name: "Occupation",
+    accessor: "occupation",
+    render: (x: any) => x.occupation,
+  },
 ];
 
 function App() {
   return (
-    <main className="container mx-auto mt-8">
-      <h1 className="text-5xl font-thin">DataTable</h1>
-
-      <DataTable data={dataTableData} />
+    <main className="flex dark:bg-black">
+      <div className="container mx-auto mt-8 h-full min-h-screen">
+        <h1 className="text-5xl font-thin dark:text-white">DataTable</h1>
+        <DataTable data={dataTableData} columns={columns} pagination={true} />
+      </div>
     </main>
   );
 }
--- src/Datatable.tsx
+++ src/Datatable.tsx
@@ -3,25 +3,40 @@ import ChevronDoubleLeft from "./icons/chevron-double-left.svg";
 import ChevronLeft from "./icons/chevron-left.svg";
 import ChevronDoubleRight from "./icons/chevron-double-right.svg";
 import ChevronRight from "./icons/chevron-right.svg";
+import ChevronUpDown from "./icons/chevron-up-down.svg";
+import ChevronUp from "./icons/chevron-up.svg";
+import ChevronDown from "./icons/chevron-down.svg";
 
-interface IDataTableProps<T extends Record<string, any>> {
+export interface IDataTableProps<T extends Record<string, any>> {
   data: T[];
+  columns: IColumnProps<T>[];
+  pagination?: boolean;
+}
+
+export interface IColumnProps<T> {
+  id: string | number;
+  name: string;
+  accessor: string;
+  sortable?: boolean;
+  render?: (data: T, index: number) => React.ReactNode;
 }
 
 function DataTable<T extends Record<string, any>>({
   data,
+  columns,
+  pagination = false,
 }: IDataTableProps<T>) {
   const [pageSize, setPageSize] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
+  const [sort, setSort] = useState<{
+    key: string | number;
+    direction: "asc" | "desc";
+  } | null>(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const columns = data[0] ? Object.keys(data[0]) : [];
+  // const simpleColumns = data[0] ? Object.keys(data[0]) : [];
 
   const numPages = Math.ceil(data.length / pageSize);
-
-  const itemsToDisplay = data.slice(
-    (currentPage - 1) * pageSize,
-    currentPage * pageSize,
-  );
 
   const handlePageSizeChange = (
     event: React.ChangeEvent<HTMLSelectElement>,
@@ -44,79 +59,178 @@ function DataTable<T extends Record<string, any>>({
     setCurrentPage(Math.min(Math.max(currentPage + page, 1), numPages));
   }
 
+  function handleSort(key: string | number) {
+    setSort((sort) => {
+      if (sort?.key === key) {
+        return { key, direction: sort.direction === "asc" ? "desc" : "asc" };
+      }
+      return { key, direction: "asc" };
+    });
+  }
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchTerm(event.target.value);
+  }
+
+  const filteredData = searchTerm
+    ? data.filter((item) => {
+        return columns.some((column) => {
+          return String(item[column.accessor])
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase());
+        });
+      })
+    : data;
+
+  const sortedData = [...filteredData].sort((a, b) => {
+    if (!sort) return 1;
+    if (sort?.direction === "asc") {
+      return a[sort.key] > b[sort.key] ? 1 : -1;
+    } else {
+      return a[sort.key] < b[sort.key] ? 1 : -1;
+    }
+  });
+
+  const itemsToDisplay = pagination
+    ? sortedData.slice((currentPage - 1) * pageSize, currentPage * pageSize)
+    : sortedData;
+
   return (
-    <table className="w-full">
-      <thead>
-        <tr className="min-h-[52px] w-full border-b border-solid border-gray-200">
-          {columns.map((column, index) => (
-            <th key={index} className="p-2 text-left capitalize text-gray-600">
-              {column}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {itemsToDisplay.map((row, rowIndex) => (
-          <tr
-            key={rowIndex}
-            className="min-h-[48px] border-b border-solid border-gray-200"
-          >
-            {columns.map((column, columnIndex) => (
-              <td className="p-2" key={columnIndex}>
-                {row[column]}
-              </td>
-            ))}
+    <div className="w-full overflow-x-auto border border-gray-200 p-4">
+      <div className="flex flex-row-reverse">
+        <input
+          type="search"
+          placeholder="Search.."
+          className=" border-b border-gray-800 outline-none dark:border-white dark:bg-black"
+          onChange={handleSearch}
+        />
+      </div>
+      <table className=" w-full flex-col overflow-x-scroll whitespace-nowrap  bg-white text-black dark:bg-black dark:text-white">
+        <thead className="">
+          <tr className="min-h-[52px] w-full border-b border-solid border-gray-800  text-lg dark:border-gray-400">
+            {columns.map((column, index) =>
+              column.sortable ? (
+                <th
+                  key={index}
+                  className="flex cursor-pointer items-center p-2 text-left capitalize text-gray-600 dark:text-gray-200"
+                  onClick={() => handleSort(column.accessor)}
+                >
+                  {column.name}
+                  {!sort ? (
+                    <ChevronUpDown />
+                  ) : sort.direction === "asc" ? (
+                    <ChevronUp />
+                  ) : (
+                    <ChevronDown />
+                  )}
+                </th>
+              ) : (
+                <th
+                  key={index}
+                  className="p-2 text-left capitalize text-gray-600 dark:text-gray-200"
+                >
+                  {column.name}
+                </th>
+              ),
+            )}
           </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        <div className="flex">
-          <span>
-            Rows per page:
-            <select onChange={handlePageSizeChange} value={pageSize}>
-              <option value="5">5</option>
-              <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="30">30</option>
-            </select>
-          </span>
-          <span>
-            {(currentPage - 1) * pageSize + 1} -{" "}
-            {Math.min(currentPage * pageSize, data.length)} of {data.length}
-          </span>
-          <span className="flex">
-            <button
-              disabled={currentPage === 1}
-              className={currentPage === 1 ? "text-gray-400" : ""}
-              onClick={() => goto("first")}
-            >
-              <ChevronDoubleLeft />
-            </button>
-            <button
-              disabled={currentPage === 1}
-              className={currentPage === 1 ? "text-gray-400" : ""}
-              onClick={() => goto(-1)}
-            >
-              <ChevronLeft />
-            </button>
-            <button
-              disabled={currentPage === numPages}
-              className={currentPage === numPages ? "text-gray-400" : ""}
-              onClick={() => goto(1)}
-            >
-              <ChevronRight />
-            </button>
-            <button
-              disabled={currentPage === numPages}
-              className={currentPage === numPages ? "text-gray-400" : ""}
-              onClick={() => goto("last")}
-            >
-              <ChevronDoubleRight />
-            </button>
-          </span>
-        </div>
-      </tfoot>
-    </table>
+        </thead>
+        <tbody>
+          {itemsToDisplay.length ? (
+            itemsToDisplay.map((row, rowIndex) => (
+              <tr
+                key={rowIndex}
+                className="min-h-[48px] border-b border-solid border-gray-200 dark:border-gray-400"
+              >
+                {columns.map((column, columnIndex) => (
+                  <td className="p-2" key={columnIndex}>
+                    {column.render
+                      ? column.render(row, columnIndex)
+                      : row[column.accessor]}
+                  </td>
+                ))}
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td className="p-2 text-center" colSpan={columns.length}>
+                No Items To display...
+              </td>
+            </tr>
+          )}
+        </tbody>
+        <tfoot>
+          {pagination && (
+            <tr>
+              <td colSpan={itemsToDisplay.length}>
+                <div className="mt-2 flex w-full flex-row-reverse p-2">
+                  <span className="ml-4">
+                    Rows per page:
+                    <select
+                      className="dark:bg-black"
+                      onChange={handlePageSizeChange}
+                      value={pageSize}
+                    >
+                      <option value="5">5</option>
+                      <option value="10">10</option>
+                      <option value="20">20</option>
+                      <option value="30">30</option>
+                    </select>
+                  </span>
+                  <span className="ml-4">
+                    {(currentPage - 1) * pageSize + 1} -{" "}
+                    {Math.min(currentPage * pageSize, itemsToDisplay.length)} of{" "}
+                    {itemsToDisplay.length}
+                  </span>
+                  <span className="flex">
+                    <button
+                      disabled={currentPage === 1}
+                      className={
+                        currentPage === 1 ? "text-gray-400" : "dark:text-white"
+                      }
+                      onClick={() => goto("first")}
+                    >
+                      <ChevronDoubleLeft />
+                    </button>
+                    <button
+                      disabled={currentPage === 1}
+                      className={
+                        currentPage === 1 ? "text-gray-400" : "dark:text-white"
+                      }
+                      onClick={() => goto(-1)}
+                    >
+                      <ChevronLeft />
+                    </button>
+                    <button
+                      disabled={currentPage === numPages}
+                      className={
+                        currentPage === numPages
+                          ? "text-gray-400"
+                          : "dark:text-white"
+                      }
+                      onClick={() => goto(1)}
+                    >
+                      <ChevronRight />
+                    </button>
+                    <button
+                      disabled={currentPage === numPages}
+                      className={
+                        currentPage === numPages
+                          ? "text-gray-400"
+                          : "dark:text-white"
+                      }
+                      onClick={() => goto("last")}
+                    >
+                      <ChevronDoubleRight />
+                    </button>
+                  </span>
+                </div>
+              </td>
+            </tr>
+          )}
+        </tfoot>
+      </table>
+    </div>
   );
 }
 
